a2a3807fce1bdf7ea5a10ea43be871b1805aa1e4
k-737 <kamronalimov80@gmail.com>
2021-10-11 05:19:08 +0500

    Map fixed

--- src/components/Catalog/index.jsx
+++ src/components/Catalog/index.jsx
@@ -1,41 +1,57 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   GoogleMap,
   Marker,
   useJsApiLoader,
   Autocomplete,
 } from "@react-google-maps/api";
-import { Containeter, Wrapper } from "./style";
+import {
+  Button,
+  Containeter,
+  Input,
+  InputWrapper,
+  Sidebar,
+  Wrapper,
+} from "./style";
+
+const libraries = ["places"];
 
 export const Catalog = () => {
+  const [place, setPlace] = useState(null);
+  const [center, setCenter] = useState({ lat: 41.2995, lng: 69.2401 });
+
   const { isLoaded } = useJsApiLoader({
     googleMapsApiKey: "AIzaSyDqw3adF1rAFs5-n2G1Emu4Z90ce31tOto",
     id: "fast - food",
-    // libraries,
+    libraries,
+    center,
   });
 
   return (
     <Containeter>
-      <Wrapper />
-      <Autocomplete>
-        <input type="text" />
-      </Autocomplete>
-      <GoogleMap
-        zoom={13}
-        center={{ lat: 41.2995, lng: 69.2401 }}
-        mapContainerStyle={{
-          width: "1500px",
-          height: "800px",
-          border: "5px solid white",
-          marginLeft: "50px",
-          marginTop: "50px",
-          // display: "flex",
-          // alignItems: "center",
-          // justifyContent: "center",
-        }}
-      >
-        <Marker position={{ lat: 41.2995, lng: 69.2401 }} />
-      </GoogleMap>
+      <Sidebar />
+      <Wrapper>
+        <InputWrapper>
+          <Autocomplete>
+            <Input />
+          </Autocomplete>
+          <Button>Search</Button>
+        </InputWrapper>
+        <GoogleMap
+          zoom={13}
+          center={center}
+          mapContainerStyle={{
+            width: "1500px",
+            height: "750px",
+            border: "5px solid white",
+            // display: "flex",
+            // alignItems: "center",
+            // justifyContent: "center",
+          }}
+        >
+          <Marker position={{ lat: 41.2995, lng: 69.2401 }} />
+        </GoogleMap>
+      </Wrapper>
     </Containeter>
   );
 };
--- src/components/Catalog/style.js
+++ src/components/Catalog/style.js
@@ -3,10 +3,36 @@ import styled from "styled-components";
 export const Containeter = styled.div`
   display: flex;
   flex-direction: column;
+  width: 100%;
 `;
 export const Wrapper = styled.div`
   width: 100%;
+  padding: 30px;
+`;
+export const Sidebar = styled.div`
+  width: 100%;
+  padding: 30px;
   height: 80px;
   background: #ffffff;
   border: 2px solid #edeff3;
+`;
+export const InputWrapper = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+export const Input = styled.input`
+  width: 1300px;
+  height: 30px;
+  outline: none;
+  border: 1px solid #1a73e8;
+  padding: 5px;
+`;
+export const Button = styled.button`
+  width: 200px;
+  height: 30px;
+  border: 1px solid #1a73e8;
+  background: #1a73e8;
+  color: white;
+  cursor: pointer;
 `;
