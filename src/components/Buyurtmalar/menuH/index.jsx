3c33e24f4d9f2661e6debcbb31ffc3ce795066d3
k-737 <kamronalimov80@gmail.com>
2021-10-27 19:55:17 +0500

    project changed

--- src/components/Buyurtmalar/menuH/index.jsx
+++ src/components/Buyurtmalar/menuH/index.jsx
@@ -2,17 +2,20 @@ import React from "react";
 import ProductCardH from "../../ProductCardH";
 import { Container } from "./style";
 import { BuyurtmaContext } from "../../../context/buyurtma";
+import Fade from "react-reveal/Fade";
 
 export const Body = () => {
   const [card] = BuyurtmaContext();
 
   return (
     <Container>
-      {/* <Wrapper> */}
-      {card.yangi.map((value) => (
-        <ProductCardH value={value} key={value.id} />
-      ))}
-      {/* </Wrapper> */}
+      <Fade top>
+        {/* <Wrapper> */}
+        {card.yangi.map((value) => (
+          <ProductCardH value={value} key={value.id} />
+        ))}
+        {/* </Wrapper> */}
+      </Fade>
     </Container>
   );
 };
--- src/components/Buyurtmalar/menuV/index.jsx
+++ src/components/Buyurtmalar/menuV/index.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import ProductCard from "../../ProductCardV";
+import Fade from "react-reveal/Fade";
 import {
   Condition,
   Container,
@@ -17,58 +18,60 @@ export const Body = () => {
   return (
     <>
       <Container>
-        <Wrapper>
-          <Condition>
-            <div>Yangi:</div>
-            <Numbers>{[...card.yangi].length}</Numbers>
-          </Condition>
-          <SummaWrapper>
-            <Spot />
-            <Summa>12,300,000 UZS</Summa>
-          </SummaWrapper>
-          {card.yangi.map((value) => (
-            <ProductCard value={value} key={value.id} />
-          ))}
-        </Wrapper>
-        <Wrapper>
-          <Condition>
-            <div>Qabul qilingan</div>
-            <Numbers>{[...card.qabul].length}</Numbers>
-          </Condition>
-          <SummaWrapper>
-            <Spot type="blue" />
-            <Summa>12,300,000 UZS</Summa>
-          </SummaWrapper>
-          {card.qabul.map((value) => (
-            <ProductCard value={value} key={value.id} />
-          ))}
-        </Wrapper>
-        <Wrapper>
-          <Condition>
-            <div>Jo'natilgan</div>
-            <Numbers>{[...card.jonatilgan].length}</Numbers>
-          </Condition>
-          <SummaWrapper>
-            <Spot type="brown" />
-            <Summa>12,300,000 UZS</Summa>
-          </SummaWrapper>
-          {card.jonatilgan.map((value) => (
-            <ProductCard value={value} key={value.id} />
-          ))}
-        </Wrapper>
-        <Wrapper>
-          <Condition>
-            <div>yopilgan</div>
-            <Numbers>{[...card.yopilgan].length}</Numbers>
-          </Condition>
-          <SummaWrapper>
-            <Spot type="pink" />
-            <Summa>12,300,000 UZS</Summa>
-          </SummaWrapper>
-          {card.yopilgan.map((value) => (
-            <ProductCard value={value} key={value.id} />
-          ))}
-        </Wrapper>
+        <Fade >
+          <Wrapper>
+            <Condition>
+              <div>Yangi:</div>
+              <Numbers>{[...card.yangi].length}</Numbers>
+            </Condition>
+            <SummaWrapper>
+              <Spot />
+              <Summa>12,300,000 UZS</Summa>
+            </SummaWrapper>
+            {card.yangi.map((value) => (
+              <ProductCard value={value} key={value.id} />
+            ))}
+          </Wrapper>
+          <Wrapper>
+            <Condition>
+              <div>Qabul qilingan</div>
+              <Numbers>{[...card.qabul].length}</Numbers>
+            </Condition>
+            <SummaWrapper>
+              <Spot type="blue" />
+              <Summa>12,300,000 UZS</Summa>
+            </SummaWrapper>
+            {card.qabul.map((value) => (
+              <ProductCard value={value} key={value.id} />
+            ))}
+          </Wrapper>
+          <Wrapper>
+            <Condition>
+              <div>Jo'natilgan</div>
+              <Numbers>{[...card.jonatilgan].length}</Numbers>
+            </Condition>
+            <SummaWrapper>
+              <Spot type="brown" />
+              <Summa>12,300,000 UZS</Summa>
+            </SummaWrapper>
+            {card.jonatilgan.map((value) => (
+              <ProductCard value={value} key={value.id} />
+            ))}
+          </Wrapper>
+          <Wrapper>
+            <Condition>
+              <div>yopilgan</div>
+              <Numbers>{[...card.yopilgan].length}</Numbers>
+            </Condition>
+            <SummaWrapper>
+              <Spot type="pink" />
+              <Summa>12,300,000 UZS</Summa>
+            </SummaWrapper>
+            {card.yopilgan.map((value) => (
+              <ProductCard value={value} key={value.id} />
+            ))}
+          </Wrapper>
+        </Fade>
       </Container>
     </>
   );
