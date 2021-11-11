42f5c45216876ccee37b63e95d38f65007dd2658
k-737 <kamronalimov80@gmail.com>
2021-11-11 09:10:34 +0500

    bug fixed

--- src/components/Buyurtmalar/menuH/index.jsx
+++ src/components/Buyurtmalar/menuH/index.jsx
@@ -2,18 +2,15 @@ import React from "react";
 import ProductCardH from "../../ProductCardH";
 import { Container } from "./style";
 import { BuyurtmaContext } from "../../../context/buyurtma";
-import Fade from "react-reveal/Fade";
 
 export const Body = () => {
   const [card] = BuyurtmaContext();
 
   return (
     <Container>
-      <Fade top>
         {card.yangi.map((value) => (
           <ProductCardH value={value} key={value.id} />
         ))}
-      </Fade>
       
     </Container>
   );
--- src/components/ProductCardH/index.jsx
+++ src/components/ProductCardH/index.jsx
@@ -14,6 +14,7 @@ import {
   Wrapper,
 } from "./style";
 import { BuyurtmaContext } from "../../context/buyurtma";
+import Fade from "react-reveal/Fade";
 
 export const ProductCardH = ({ value }) => {
   const [card, setBuyurtmaData] = BuyurtmaContext();
@@ -22,75 +23,78 @@ export const ProductCardH = ({ value }) => {
     let filtered = card[value.category].filter((data) => data.id !== value.id);
     let newData = { ...card, [value.category]: filtered };
     setBuyurtmaData(newData);
+    console.log(newData);
   };
   return (
-    <Container>
-      <Main>
-        <Wrapper center>
-          <Info bottom>
-            <Info.OrderID>{value.orderId}</Info.OrderID>
-            <IconWrapper>
-              <Info.Save />
-            </IconWrapper>
-          </Info>
-          <BottomLine />
-          <Info>
-            <ClockWrapper>
-              <Info.Clock />
-              <Info.Time>{`${value.time.getHours()}:${value.time.getMinutes()}`}</Info.Time>
-            </ClockWrapper>
-          </Info>
-        </Wrapper>
-        <Wrapper flex>
-          <Info>
-            <Info.User />
-            <Info.Name>{value.user.name}</Info.Name>
-          </Info>
-          <Info>
-            <Info.CallIcon />
-            <Info.Number>{value.user.number}</Info.Number>
-          </Info>
-        </Wrapper>
-        <Wrapper flex>
-          <MainWrapper top>
-            <Info.ClipBoard /> <Info.ClipPrice>35,000UZS</Info.ClipPrice>{" "}
-            <Info.PaymeIcon /> <Info.Payme>Payme</Info.Payme>
-          </MainWrapper>
-          <MainWrapper>
-            <Info.Truck /> <Info.ClipPrice>5,000 UZS</Info.ClipPrice>
-          </MainWrapper>
-          <Info.TotalPrice top>Umumiy summa</Info.TotalPrice>
-          <Footer>
-            <Info.Price>{value.summa}</Info.Price>
-            <Info.Name>&nbsp;UZS</Info.Name>
-          </Footer>
-        </Wrapper>
-        <Wrapper flex last>
-          <MainWrapper between>
-            <div>
-              <Info.Operator>Operator:</Info.Operator>
-              <Info.OperatorName>{value.operator.name}</Info.OperatorName>
-            </div>
-            <div onClick={() => onCancel(value)}>
-              <Cancel />
-            </div>
-          </MainWrapper>
-          <MainWrapper between top>
-            <div>
-              <Info.Operator>Filallar:</Info.Operator>
-              <Info.OperatorName>
-                {value.filial.title} <br />
-                {value.filial.location}
-              </Info.OperatorName>
-            </div>
-            <div>
-              <Done />
-            </div>
-          </MainWrapper>
-        </Wrapper>
-      </Main>
-      <AddOrder>12345</AddOrder>
-    </Container>
+    <Fade top>
+      <Container>
+        <Main>
+          <Wrapper center>
+            <Info bottom>
+              <Info.OrderID>{value.orderId}</Info.OrderID>
+              <IconWrapper>
+                <Info.Save />
+              </IconWrapper>
+            </Info>
+            <BottomLine />
+            <Info>
+              <ClockWrapper>
+                <Info.Clock />
+                <Info.Time>{`${value.time.getHours()}:${value.time.getMinutes()}`}</Info.Time>
+              </ClockWrapper>
+            </Info>
+          </Wrapper>
+          <Wrapper flex>
+            <Info>
+              <Info.User />
+              <Info.Name>{value.user.name}</Info.Name>
+            </Info>
+            <Info>
+              <Info.CallIcon />
+              <Info.Number>{value.user.number}</Info.Number>
+            </Info>
+          </Wrapper>
+          <Wrapper flex>
+            <MainWrapper top>
+              <Info.ClipBoard /> <Info.ClipPrice>35,000UZS</Info.ClipPrice>{" "}
+              <Info.PaymeIcon /> <Info.Payme>Payme</Info.Payme>
+            </MainWrapper>
+            <MainWrapper>
+              <Info.Truck /> <Info.ClipPrice>5,000 UZS</Info.ClipPrice>
+            </MainWrapper>
+            <Info.TotalPrice top>Umumiy summa</Info.TotalPrice>
+            <Footer>
+              <Info.Price>{value.summa}</Info.Price>
+              <Info.Name>&nbsp;UZS</Info.Name>
+            </Footer>
+          </Wrapper>
+          <Wrapper flex last>
+            <MainWrapper between>
+              <div>
+                <Info.Operator>Operator:</Info.Operator>
+                <Info.OperatorName>{value.operator.name}</Info.OperatorName>
+              </div>
+              <div onClick={() => onCancel(value)}>
+                <Cancel />
+              </div>
+            </MainWrapper>
+            <MainWrapper between top>
+              <div>
+                <Info.Operator>Filallar:</Info.Operator>
+                <Info.OperatorName>
+                  {value.filial.title} <br />
+                  {value.filial.location}
+                </Info.OperatorName>
+              </div>
+              <div>
+                <Done />
+              </div>
+            </MainWrapper>
+          </Wrapper>
+        </Main>
+        {/* <AddOrder>12345</AddOrder> */}
+      </Container>
+    </Fade>
   );
 };
 export default ProductCardH;
